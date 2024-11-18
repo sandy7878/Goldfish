5321a7bdd1ede9b55245306797231c74e7523fb4
sandy7878 <38539876+sandy7878@users.noreply.github.com>
2024-11-18 16:52:41 +0530

    Add files via upload

---
+++ sandy_emulator.mk
@@ -0,0 +1,10 @@
+
+$(call inherit-product, device/google/cuttlefish/vsoc_x86/phone/aosp_cf.mk)
+
+PRODUCT_NAME := sandy_emulator
+PRODUCT_DEVICE := sandy_emulator
+PRODUCT_BRAND := sandy
+PRODUCT_MODEL := sandy on Cuttlefish Automotive
+
+
+
